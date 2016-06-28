3d8c472f02c504915a8c785bd64e08e93d5f7068
Ali Najafizadeh <a.najafizadeh@gmail.com>
2016-06-27 20:13:29 -0400

    cleaned up

--- example/WithIcons/index.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-import React, { AppRegistry } from 'react-native';
-import App from './app';
-
-AppRegistry.registerComponent('WithIcons', () => App);
--- libs/icon/index.js
+++
@@ -1,5 +0,0 @@
-import { RawIcon, extendRawIcon } from './raw';
-import Icon from './icon';
-import IconWithBar from './icon_with_bar';
-
-export { RawIcon, extendRawIcon, Icon, IconWithBar };
